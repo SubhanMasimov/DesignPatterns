59eaedd6abbacf6576f25dba386afc5c775af30e
Subhan Masimov <subhanmasimov02@gmail.com>
2021-05-22 17:24:44 +0400

    Added codes for Singleton Design Pattern

--- SingletonDesignPattern/Program.cs
+++ SingletonDesignPattern/Program.cs
@@ -6,7 +6,24 @@ namespace SingletonDesignPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Singleton singleton1 = Singleton.GetSingleton();
+            Singleton singleton2 = Singleton.GetSingleton();
+
+            string result = singleton1.Equals(singleton2) ? "equals" : "not equals";
+
+            Console.WriteLine(result);
+        }
+    }
+
+    class Singleton
+    {
+        private static Singleton _singleton;
+
+        private Singleton() { }
+
+        public static Singleton GetSingleton()
+        {
+            return _singleton == null ? _singleton = new Singleton() : _singleton;
         }
     }
 }
